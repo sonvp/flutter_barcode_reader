899f009ea2c168b5334148faf392f1c33abc343a
tranvietha2809 <tranvietha.2809@gmail.com>
2021-01-19 17:17:17 +0700

    change ios namingn

--- ios/Classes/BarcodeScanPlugin.m
+++ ios/Classes/BarcodeScanPlugin.m
@@ -1,15 +1,15 @@
 #import "BarcodeScanPlugin.h"
-#if __has_include(<barcode_scan/barcode_scan-Swift.h>)
-#import <barcode_scan/barcode_scan-Swift.h>
+#if __has_include(<barcode_scan/barcode_scan_javalite-Swift.h>)
+#import <barcode_scan/barcode_scan_javalite-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "barcode_scan-Swift.h"
+#import "barcode_scan_javalite-Swift.h"
 #endif
 
 @implementation BarcodeScanPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftBarcodeScanPlugin registerWithRegistrar:registrar];
+  [BarcodeScanPlugin registerWithRegistrar:registrar];
 }
 @end
